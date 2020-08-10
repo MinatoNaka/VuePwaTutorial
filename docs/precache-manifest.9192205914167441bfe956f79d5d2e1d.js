baf841496d8a8f7c87692982e88b85cdb6847830
aq_minato.naka <minato.naka@asia-quest.jp>
2020-08-11 07:48:45 +0900

    docsディレクトリ追加

---
+++ docs/precache-manifest.9192205914167441bfe956f79d5d2e1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6e0e280d652fb323b0",
+    "url": "/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "921b7e8978c8c6f3293f9cf1715e911e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6e0e280d652fb323b0",
+    "url": "/js/app.9bc934c3.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "248764d12875a71ef3ae4ea0a2d9070f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
